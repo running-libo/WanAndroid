90d5599933e108e35f3e65dd2abe296da94fa0a8
apple <18201497620@163.com>
2021-06-27 18:55:58 +0800

    leakcanary使用

---
+++ library-base/src/main/java/com/libo/module_base/application/BaseApplication.kt
@@ -0,0 +1,16 @@
+package com.libo.module_base.application
+
+import android.app.Application
+
+/**
+ * create by libo
+ * create on 2021/6/27
+ * description
+ */
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
