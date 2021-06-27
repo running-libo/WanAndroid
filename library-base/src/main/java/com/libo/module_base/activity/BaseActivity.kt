7fc9f09e037f0d5af8e407e5e248b8671f74fcf3
apple <18201497620@163.com>
2021-06-27 19:51:51 +0800

    baseActivity引入

---
+++ library-base/src/main/java/com/libo/module_base/activity/BaseActivity.kt
@@ -0,0 +1,52 @@
+package com.libo.module_base.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.gyf.immersionbar.ImmersionBar
+import com.libo.module_base.util.ActivityManager
+
+/**
+ * create by apple
+ * create on 2021/6/27
+ * description BaseActivity封装
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(setLayoutId())
+        statusBaySetting()
+        ActivityManager.addActivity(this)
+    }
+
+    abstract fun setLayoutId(): Int
+
+    /**
+     * 系统状态栏颜色设置
+     */
+    open fun statusBaySetting() {
+        //监听activity的onCreate生命周期事件
+        lifecycle.addObserver(object: LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                //onCreate生命周期事件回调
+                ImmersionBar.with(this@BaseActivity)
+                    .statusBarColor(setStatusBarColor())
+                    .fitsSystemWindows(true)
+                    .init()
+            }
+        })
+    }
+
+    open fun setStatusBarColor(): Int = com.libo.library_res.R.color.white
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        ActivityManager.removeActivity(this)
+    }
+}
---
+++ module-home/src/main/java/com/libo/module_home/activity/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.libo.module_home.activity
+
+import com.libo.module_base.activity.BaseActivity
+import com.libo.module_home.R
+
+/**
+ * create by libo
+ * create on 2021/6/27
+ * description 主页面
+ */
+class HomeActivity : BaseActivity() {
+
+    override fun setLayoutId(): Int = R.layout.activity_home
+
+}
